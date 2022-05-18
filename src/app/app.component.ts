3c4a0dfa871fbaf510becb6a88edc23ef07b3c16
williamdc92 <guglielmodc@gmail.com>
2022-05-18 09:01:18 +0200

    commenting viewchild section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,41 +26,41 @@ export class AppComponent {
   constructor(private auth: AuthService, private router: Router) {}
   
 
-  ngAfterViewInit() {
-    if (this.auth.isLogged) {
-      this.setstate.nativeElement.textContent = "LogOut";
-    } 
-     else {
-       this.setstate.nativeElement.textContent = "LogIn";
-       }
-  }
+  // ngAfterViewInit() {
+  //   if (this.auth.isLogged) {
+  //     this.setstate.nativeElement.textContent = "LogOut";
+  //   } 
+  //    else {
+  //      this.setstate.nativeElement.textContent = "LogIn";
+  //      }
+  // }
 
-  onChangeColor = () => {
-    const currentcolor = this.main!.nativeElement.style.backgroundColor;
-    let newcolor = "";
-    if (currentcolor == "rgba(255, 255, 255, 0.78)") {
-      this.changecolor.nativeElement.textContent = "WhiteMode";
-      newcolor = "rgb(0, 0, 0)";
-    } 
-    else {
-      this.changecolor.nativeElement.textContent = "DarkMode";
-      newcolor = "rgba(255, 255, 255, 0.78)"
-    } 
-    this.main.nativeElement.style.backgroundColor=newcolor;
-  }
+  // onChangeColor = () => {
+  //   const currentcolor = this.main!.nativeElement.style.backgroundColor;
+  //   let newcolor = "";
+  //   if (currentcolor == "rgba(255, 255, 255, 0.78)") {
+  //     this.changecolor.nativeElement.textContent = "WhiteMode";
+  //     newcolor = "rgb(0, 0, 0)";
+  //   } 
+  //   else {
+  //     this.changecolor.nativeElement.textContent = "DarkMode";
+  //     newcolor = "rgba(255, 255, 255, 0.78)"
+  //   } 
+  //   this.main.nativeElement.style.backgroundColor=newcolor;
+  // }
 
-  changeState() {
-    if (this.auth.isLogged) {
-      this.setstate.nativeElement.textContent = "LogIn";
-      this.navbar?.logOut();
-    }
-    else {
-      this.setstate.nativeElement.textContent = "LogOut";
-      this.router.navigate(['/auth']);
+  // changeState() {
+  //   if (this.auth.isLogged) {
+  //     this.setstate.nativeElement.textContent = "LogIn";
+  //     this.navbar?.logOut();
+  //   }
+  //   else {
+  //     this.setstate.nativeElement.textContent = "LogOut";
+  //     this.router.navigate(['/auth']);
 
-    }
+  //   }
     
-  }
+  // }
 
 
   
